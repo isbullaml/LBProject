34d95025bf606ba6ba4aa750d58968a579c7e18e
Mohammed Isbulla <isbulla@jrcsoftware.com>
2026-04-02 22:38:10 +0400

    Created the breach controller

---
+++ Controllers/BreachController.cs
@@ -0,0 +1,38 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using LBProject.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LBProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BreachController : ControllerBase
+    {
+        private IBreachRepository _breachRepository;
+        public BreachController(IBreachRepository breachRepository)
+        {
+            _breachRepository = breachRepository;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Breach>>> GetAll()
+        {
+            IEnumerable<Breach> breaches = await _breachRepository.GetAllAsync();
+            List<BreachDto> result = BreachDto.FromEntityList(breaches);
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BreachDto>> GetByid(string id)
+        {
+            Breach breach = await _breachRepository.GetByIdAsync(id);
+            if (breach == null)
+            {
+                return NotFound();
+            }
+            BreachDto dto = BreachDto.FromEntity(breach);
+            return Ok(dto);
+        }
+    }
+}
---
+++ Dto/BreachDto.cs
@@ -0,0 +1,56 @@
+﻿using Core.Entities;
+
+namespace LBProject.Dto
+{
+    public class BreachDto
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Domain { get; set; }
+        public DateTime BreachDate { get; set; }
+        public long PwnCount { get; set; }
+        public string Description { get; set; }
+        public List<string> DataClasses { get; set; }
+
+        public string LogoPath { get; set; }
+        public string Attribution { get; set; }
+        public string DisclosureUrl { get; set; }
+
+        public static BreachDto FromEntity(Breach breach)
+        {
+            BreachDto dto = new BreachDto();
+            dto.Id = breach.Id; 
+            dto.Title = breach.Title;
+            dto.Domain = breach.Domain;
+            dto.BreachDate = breach.BreachDate;
+            dto.PwnCount = breach.PwnCount;
+            dto.Description = breach.Description;
+
+            dto.DataClasses = new List<string>();
+            if (breach.DataClasses != null)
+            {
+                foreach (string s in breach.DataClasses)
+                {
+                    dto.DataClasses.Add(s);
+                }
+            }
+
+            dto.LogoPath = breach.LogoPath;
+            dto.Attribution = breach.Attribution;
+            dto.DisclosureUrl = breach.DisclosureUrl;
+
+            return dto;
+        }
+
+        // Map list of Breach to list of BreachDto
+        public static List<BreachDto> FromEntityList(IEnumerable<Breach> breaches)
+        {
+            List<BreachDto> dtoList = new List<BreachDto>();
+            foreach (Breach b in breaches)
+            {
+                dtoList.Add(FromEntity(b));
+            }
+            return dtoList;
+        }
+    }
+}
