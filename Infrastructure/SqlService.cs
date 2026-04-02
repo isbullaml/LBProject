f8685b5ef559b3c133b099a761d0d1f692af0032
Mohammed Isbulla <isbulla@jrcsoftware.com>
2026-04-02 21:33:55 +0400

    Setup the project with DB and Core

---
+++ Infrastructure/SqlService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure
+{
+    public class SqlService
+    {
+        private readonly string _connectionString;
+
+        public SqlService(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+    }
+}
