92109b1e265944ac36a2653301be57794120b027
Mohammed Isbulla <isbulla@jrcsoftware.com>
2026-04-03 00:29:06 +0400

    api prefix added in url

--- Controllers/BreachesController.cs
+++ Controllers/BreachesController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace LBProject.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class BreachesController : ControllerBase
     {
         private IBreachRepository _breachRepository;
